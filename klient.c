10630a86b6b3806b6281aa4aac7f63c49d418a6a
panka134 <134panka@gmail.com>
2020-12-10 11:37:47 +0100

    more messages

--- klient.c
+++ klient.c
@@ -14,18 +14,32 @@ int login(int server){
   int my_id = msgget(12341, 0644|IPC_CREAT); //TODO: nowy numer
   login_msg.type = 1;
   login_msg.number = 12341;
-  strcpy(login_msg.text, "my_name");
+  strcpy(login_msg.text, "Obi Wan");
   msgsnd(server, &login_msg, sizeof(login_msg)-sizeof(long), 0);
   return my_id;
 }
 
-void register_sub(int topic, int sublen){ //-1 - forever
+void register_sub(int server, int topic, int sublen){ //-1 -> forever
+  struct msgbuf sub_msg;
+  sub_msg.type = 3;
+  sub_msg.number = topic;
+  msgsnd(server, &sub_msg, sizeof(sub_msg)-sizeof(long), 0);
 }
 
-void register_topic(int topic){
+void register_topic(int server, int topic, char name[30]){
+  struct msgbuf topic_msg;
+  topic_msg.type = 2;
+  topic_msg.number = topic;
+  strcpy(topic_msg.text, name);
+  msgsnd(server, &topic_msg, sizeof(topic_msg)-sizeof(long), 0);
 }
 
-void send_msg(){
+void send_msg(int server, int topic, char msg_text[1024]){
+  struct msgbuf message;
+  message.type = 4;
+  message.number = topic;
+  strcpy(message.text, msg_text);
+  msgsnd(server, &message, sizeof(message)-sizeof(long), 0);
 }
 
 void receive_msg_sync(){
@@ -34,9 +48,16 @@ void receive_msg_sync(){
 void receive_msg_async(){ //???
 }
 
+void shutdown(int server){
+  struct msgbuf message;
+  message.type = 5;
+  msgsnd(server, &message, sizeof(message)-sizeof(long), 0);
+}
+
 int main(int argc, char *argv[]) {
   int server = msgget(12340, 0);
   int me = login(server);
+  shutdown(server);
 
   return 0;
 }
--- serwer.c
+++ serwer.c
@@ -2,11 +2,13 @@
 //typ2 - nowy temat
 //typ3 - zapis na subskrybcję
 //typ4 - nowa wiadomość
+//typ5 - wyłączenie systemu
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 
 struct client{
@@ -18,7 +20,7 @@ struct topic{
   //czy jest przypisany do konkretnego klienta czy wszyscy mogą w nim pisać?
   int id;
   char name[30];
-  int subs[10][2]; //id, lenght
+  int subs[10][2]; //id, length (-1 -> forever)
 };
 
 struct msgbuf{
@@ -29,11 +31,11 @@ struct msgbuf{
 
 
 void login(struct msgbuf *message, struct client* client_place){
-  printf("Hello there\n");
   struct client client;
-  client.id = message->number; //TODO
+  client.id = msgget(message->number, 0644|IPC_CREAT);
   strcpy(client.name, message->text);
   *client_place = client;
+  printf("Hello %s\n", client.name);
 }
 
 void add_sub(){
@@ -46,7 +48,13 @@ void send_msgs(){
   //czy powinien wysyłać wiadomość, do klienta, który ją wysłał?
 }
 
-void listen(){
+void shutdown(int me, struct client* first_client, struct client* last_client){
+  for (;last_client >= first_client; last_client--){
+    msgctl(last_client->id, IPC_RMID, NULL);
+    printf("Closed %s\n", last_client->name);
+  }
+  msgctl(me, IPC_RMID, NULL);
+  printf("Goodbye!\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -56,16 +64,17 @@ int main(int argc, char *argv[]) {
   struct client* next_client = clients;
   struct topic* next_topic = topics;
   struct msgbuf message;
-  int id = msgget(12340, 0644|IPC_CREAT);
+  int my_id = msgget(12340, 0644|IPC_CREAT);
+  bool running = true;
 
-
-  while (1) {
-    msgrcv(id, &message, sizeof(message)-sizeof(long), 0, 0);
+  while (running) {
+    msgrcv(my_id, &message, sizeof(message)-sizeof(long), 0, 0);
     switch (message.type) {
-      case 1:
-        login(&message, next_client++);
-        printf("%s %d\n", (next_client-1)->name, next_client);
-        break;
+      case 1: login(&message, next_client++); break;
+      case 2: add_topic(); break;
+      case 3: add_sub(); break;
+      case 4: send_msgs(); break;
+      case 5: shutdown(my_id, clients, next_client-1); running = false; break;
     }
 
   }
